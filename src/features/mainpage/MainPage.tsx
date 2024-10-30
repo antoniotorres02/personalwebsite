84200be094776bdd45d8dd69d4fa5a6035af1241
antoniotorres02 <torresbordonantonio@gmail.com>
2024-10-30 19:25:40 +0100

    FEAT: Added Routes

---
+++ src/features/mainpage/MainPage.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Header from './components/Header'
+import About from './components/About'
+import Skills from './components/skills/Skills'
+import Experience from './components/experience/Experience'
+import Projects from './components/projects/Projects'
+import Education from './components/education/Education'
+
+export default function MainPage() {
+  return (
+    
+    <div className="container">
+      <Header />
+      <About />
+      <Skills />
+      <Experience />
+      <Projects />
+      <Education />
+      {/*
+      TODO: Bind the Contact component to my mail server and send the form data to my email
+      <Contact />
+      */} 
+    </div>
+  )
+}
